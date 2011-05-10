65e4eb0dc5579bc17ba2a0587bfcdf2488f5ac3f
grinner <a.grinner@gmail.com>
2011-05-10 11:39:31 -0400

    began a build script for install

---
+++ build_main.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+# Build instructions for Polonator Java source, as an alternative to netbeans
+# puts classes in a jar file and cleans up afterwards
+#
+
+SCRIPT=$(readlink -f $0) # doesn't work for os x
+SCRIPT=`python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
+#SCRIPT=`perl -e 'use Cwd "abs_path";print abs_path(shift)' $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+cd $ROOTPATH
+cd polonator
+make
+
