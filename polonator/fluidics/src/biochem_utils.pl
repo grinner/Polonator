9b119b9f53cf5f127de090cd1bb36882627237a5
grinner <a.grinner@gmail.com>
2011-05-23 10:08:05 -0400

    couple changes

--- polonator/fluidics/src/biochem_utils.pl
+++ polonator/fluidics/src/biochem_utils.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 
 $cmd = join(' ', @ARGV);
-$cmd = "python /home/polonator/G.007/G.007_fluidics/src/biochem_utils.py " . $cmd;
+# $cmd = "python /home/polonator/G.007/G.007_fluidics/src/biochem_utils.py " . $cmd;
+$cmd = "python ' . $ENV{'POLONATOR_PATH'} . '/polonator/fluidics/src/biochem_utils.py " . $cmd;
 print "$cmd\n";
 system "$cmd";
 
